2b5677619632ea1880078b08c5bce38234cd6eb9
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-02 11:47:37 +0100

    structure functions

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function runGame ()
+
+function checkAnswer()
+
+function calculateCorrectAnswer()
+
+function incrementScore()
+
+function incrementWrongAnswer()
+
+function displayAdditionQuestion()
+
+function displaySubstractQuestion()
+
+function displayMultiplyQuestion()
+
+function displayDivideQuestion()

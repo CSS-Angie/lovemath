90abb297a4fe5160e7b5b97e8a728c94ebd4bed9
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-02 17:06:06 +0100

    first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// wait for the dom to finish loading before running the game
+// get the button elelments and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("you clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+        }}
+    })
+
 function runGame ()
 
 function checkAnswer()

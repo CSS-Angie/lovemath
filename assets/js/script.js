fa8f19d6a52d734c3bc619a6703e4f3e2bb875b8
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-03 21:03:07 +0100

    bigger than for division and subtract

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,8 +97,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubstractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1: operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
     document.getElementById('operator').textContent = "-";
 }
 
@@ -109,8 +109,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1: operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
     document.getElementById('operator').textContent = "/";
 }
 

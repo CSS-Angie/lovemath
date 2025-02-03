a49f6a5b0f71aeba0d3b4792e8672d708bc7aa9e
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-03 11:27:13 +0100

    number selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type")=== "submit") {
-                alert("you clicked submit");
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+            }
             });
         }
     });
+/**
+ * main game "loop", called when the script is first loaded 
+ * and after the user's answer has been processed
+ */
 
-function runGame ()
+    function runGame ()
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-function checkAnswer()
-
-function calculateCorrectAnswer()
-
-function incrementScore()
-
-function incrementWrongAnswer()
-
-function displayAdditionQuestion()
-
-function displaySubstractQuestion()
-
-function displayMultiplyQuestion()
-
-function displayDivideQuestion()
+    function checkAnswer()
+    
+    function calculateCorrectAnswer()
+    
+    function incrementScore()
+    
+    function incrementWrongAnswer()
+    
+    function displayAdditionQuestion()
+    
+    function displaySubstractQuestion()
+    
+    function displayMultiplyQuestion()
+    
+    function displayDivideQuestion()
+    

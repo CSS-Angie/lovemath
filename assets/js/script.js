a454c07a4febf5c266f12b28e59bb12292fbf168
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-03 12:10:17 +0100

    JS calculate answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,65 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
             });
         }
+        runGame("addition");
     });
 /**
  * main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
  */
 
-    function runGame ()
+    function runGame (gameType) {
+
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-    function checkAnswer()
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unkown game type; ${gameType}`);
+        throw `Unknown game type: {gameType}. Aborting.`
+    }
+
+    }
+
+    function displayAdditionQuestion(operand1, operand2) {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
+    }
+    
+/**
+ * Gets the operands - the numbers - and the operator (plus, minus, etc)
+ * directly from the DOM and returns a correct answer
+ */
+    
+    function checkAnswer() {
+
     
     function calculateCorrectAnswer()
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+    if (operator === +) {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`unimplemented operator ${operator}`)
+        throw `Unimplemented operator ${operator}`;
+    }
+}
     
     function incrementScore()
     
     function incrementWrongAnswer()
     
-    function displayAdditionQuestion()
     
     function displaySubstractQuestion()
     
     function displayMultiplyQuestion()
     
     function displayDivideQuestion()
-    
